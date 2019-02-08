b65d68e80d215c56eab4371e08428fd66e810c1b
Ralf Hergert <ralf.hergert@gmx.de>
2019-02-08 10:34:21 +0100

    added stub for the Druid ability

---
+++ src/main/java/de/ralfhergert/dota2/autochess/classes/Druid.java
@@ -0,0 +1,12 @@
+package de.ralfhergert.dota2.autochess.classes;
+
+import de.ralfhergert.dota2.autochess.ability.Ability;
+
+/**
+ * Druid class allows Druid to upgrade earlier.
+ *  - 2 different Druid classes allow two level-1-druids to upgrade
+ *  - 4 different Druid classes allow two level-2-druids to upgrade
+ *
+ *  The ability is not yet implemented.
+ */
+public class Druid extends Ability {}
--- src/main/java/de/ralfhergert/dota2/autochess/hero/Furion.java
+++ src/main/java/de/ralfhergert/dota2/autochess/hero/Furion.java
@@ -3,6 +3,7 @@ package de.ralfhergert.dota2.autochess.hero;
 import de.ralfhergert.dota2.autochess.ability.AutoAttackAbility;
 import de.ralfhergert.dota2.autochess.ability.ManaRegeneration;
 import de.ralfhergert.dota2.autochess.character.Character;
+import de.ralfhergert.dota2.autochess.classes.Druid;
 import de.ralfhergert.dota2.autochess.progression.DoubleEachLevel;
 import de.ralfhergert.dota2.autochess.species.Elf;
 
@@ -11,7 +12,7 @@ public class Furion extends Character {
     public Furion(String team, int level) {
         super(team, new DoubleEachLevel(level).of(500), 0, 0);
         addAbility(new Elf());
-        // TODO addAbility(new Druid());
+        addAbility(new Druid());
         addAbility(new AutoAttackAbility(new DoubleEachLevel(level).of(45), new DoubleEachLevel(level).of(50), (long)(1000/1.4), 400));
         addAbility(new ManaRegeneration());
         // TODO addAbility(?);
