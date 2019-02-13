5c94e9a638720b3cbdb8813307aada6d02c1760c
Ralf Hergert <ralf.hergert@haufe-lexware.com>
2019-02-13 14:36:58 +0100

    removed unused import statement

--- src/main/java/de/ralfhergert/dota2/autochess/ability/DamageAbility.java
+++ src/main/java/de/ralfhergert/dota2/autochess/ability/DamageAbility.java
@@ -1,7 +1,5 @@
 package de.ralfhergert.dota2.autochess.ability;
 
-import de.ralfhergert.dota2.autochess.character.Character;
-
 /**
  * This is a marker interface for abilities dealing damage.
  */
